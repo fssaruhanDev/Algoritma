d8c6b04b549f540a0b6333cbd9ca7a8afae86e0c
fssaruhanDev <83281184+fssaruhanDev@users.noreply.github.com>
2024-01-25 09:08:37 +0300

    Add project files.

---
+++ Algoritma/Program.cs
@@ -0,0 +1,37 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Tree("azoppoza");
+        Console.WriteLine();
+        Tree("abccddbeea");
+    }
+
+    public static void Tree(string input)
+    {
+        var stack = new Stack<char>();
+        int currentLevel = 0;
+
+        foreach (var c in input)
+        {
+            if (stack.Count > 0 && stack.Peek() == c)
+            {
+                stack.Pop();
+                currentLevel--;
+            }
+            else
+            {
+                for (int i = 0; i < currentLevel; i++)
+                {
+                    Console.Write('-');
+                }
+                Console.WriteLine(c);
+                stack.Push(c);
+                currentLevel++;
+            }
+        }
+    }
+
+
+}
